825e1b039bea2806eeced33c036e3f6adc1eaa87
Inochi Amaoto <windindex@163.com>
2019-02-08 17:39:23 +0800

    add namespace change

--- include/utility/config/debug/utility_debug_config.hpp
+++ include/utility/config/debug/utility_debug_config.hpp
@@ -2,6 +2,8 @@
 #ifndef ___UTILITY__DEBUG__CONFIG___
 #define ___UTILITY__DEBUG__CONFIG___
 
+#define UTILITY_DEBUG
+
 #ifdef UTILITY_DEBUG
 
 #define __utility_private public
@@ -16,6 +18,11 @@
 #define UTILITY_MEMORY_HAS_EXTRA true
 #define UTILITY_TRAIT_HAS_EXTRA true
 
+#define __utility_interspace_start(_name) namespace _name
+#define __utility_globalspace_start(_name) namespace _name {
+#define __utility_interspace_end(_name)
+#define __utility_globalspace_end(_name) }
+
 #ifndef UTILITY_NO_EXTRA_CHECK
 
 #else
@@ -27,6 +34,12 @@
 #define __utility_private private
 #define __utility_debug   private
 #define __UTILITY_ASSGIN(__TYPE, __TYPENAME, __ASS)
+
+#define __utility_interspace_start(_name) inline namespace _name
+#define __utility_globalspace_start(_name) namespace std { \
+  inline namespace _name {
+#define __utility_interspace_end(_name)
+#define __utility_globalspace_end(_name) } }
 
 #endif // ! __UTILITY_DEBUG
 
